a4fde480fddc566c39b6f4817da7f5d0520d5caf
Xolani Mazibuko <mazi76erx@gmail.com>
2018-08-31 15:22:42 +0200

    Initial Commit

---
+++ scripts/carousel.js
@@ -0,0 +1,12 @@
+//
+//  Portfolio
+//  carousel.js
+//
+$(document).ready(function () {
+    
+    $('.experiences-carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+    });
+});
---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+//
+//  Portfolio
+//  main.JS
+//
+$(document).ready(function () {
+
+    // When email link is pressed then open mail client
+    $('#email-btn').click(function () {
+        window.open('mailto:mazi76erx@gmail.com?subject=Your subject');
+    });
+
+    /**
+     *
+     * Handles the animation for different elements
+     *
+     */
+})
