9a19ae778d3a905b0b9f00bd4e244c9abeb33683
Xolani <mazi76erx@gmail.com>
2025-03-06 15:25:36 +0000

    Removed serverside props

--- pages/articles/index.jsx
+++ pages/articles/index.jsx
@@ -1,36 +1,13 @@
-import Recent from '../../components/sections/articles/recent'
 import Color from '../../components/utils/page.colors.util'
 
 import colors from '../../content/articles/_colors.json'
 import settings	from '../../content/_settings.json'
 
 //
-export default function Articles({ mediumArticles }) {
+export default function Articles() {
 	return (
 		<>
 			<Color colors={colors} />
-			<Recent mediumArticles={mediumArticles}/>
 		</>
 	)
-}
-
-// This gets called on every request
-export async function getStaticSideProps({ res }) {
-
-	res.setHeader(
-		'Cache-Control',
-		'public, s-maxage=600, stale-while-revalidate=59'
-	)
-
-	console.log(settings.username.medium)
-
-	const [ mediumRSS ] = await Promise.all( [
-		fetch(`https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/${settings.username.medium}`),
-	] )
-	
-	let [ mediumArticles ] = await Promise.all( [
-		mediumRSS.json(),
-	] )
-
-	return { props: { mediumArticles } }
 }
