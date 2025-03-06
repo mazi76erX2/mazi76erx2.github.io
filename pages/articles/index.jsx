4c3c0d4459d000614d3af668e860b6bbde9451be
Xolani <mazi76erx@gmail.com>
2025-03-06 15:17:39 +0000

    Removed serverside props

--- pages/articles/index.jsx
+++ pages/articles/index.jsx
@@ -13,3 +13,24 @@ export default function Articles({ mediumArticles }) {
 		</>
 	)
 }
+
+// This gets called on every request
+export async function getStaticSideProps({ res }) {
+
+	res.setHeader(
+		'Cache-Control',
+		'public, s-maxage=600, stale-while-revalidate=59'
+	)
+
+	console.log(settings.username.medium)
+
+	const [ mediumRSS ] = await Promise.all( [
+		fetch(`https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/${settings.username.medium}`),
+	] )
+	
+	let [ mediumArticles ] = await Promise.all( [
+		mediumRSS.json(),
+	] )
+
+	return { props: { mediumArticles } }
+}
