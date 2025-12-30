54da781e9d673cd0eb3a356ade41e23cc93f0cdb
Xolani Mazibuko <mazi76erx@gmail.com>
2025-12-30 16:06:32 +0200

    update

--- next.config.js
+++ next.config.js
@@ -6,23 +6,31 @@ const nextConfig = {
 		dir: '/',
 	},
 	images: {
-		unoptimized: true,
 		remotePatterns: [
 			{
 				protocol: 'https',
 				hostname: '**.vercel.app',
+				pathname: '/api/**'
 			},
 			{
 				protocol: 'https',
 				hostname: '**.shields.io',
+				pathname: '/badge/**'
+			},
+			{
+				protocol: 'https',
+				hostname: '**.shields.io',
+				pathname: '/github/**'
 			},
 			{
 				protocol: 'https',
 				hostname: '**.githubusercontent.com',
+				pathname: '/**'
 			},
 			{
 				protocol: 'https',
 				hostname: '**.medium.com',
+				pathname: '/**'
 			},
 		],
 	},
