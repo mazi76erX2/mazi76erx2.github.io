9350bcdea222ba2b970fae0ef73a1868fcf830da
Xolani Mazibuko <mazi76erx@gmail.com>
2026-05-21 11:06:09 +0200

    Fixed linting
    Installed the missing packages with bun install.
    Re-ran bun run build locally and it now completes successfully.
    Kept the earlier fix in icon.util.jsx and the dependency entries in

--- components/utils/icon.util.jsx
+++ components/utils/icon.util.jsx
@@ -2,17 +2,14 @@
 import { useEffect, useState } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 
-// Font Awesome packages
+// Font Awesome packages (use free packs to avoid pro-only imports on CI)
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { fas } from '@fortawesome/pro-solid-svg-icons';
-import { fat } from '@fortawesome/pro-thin-svg-icons';
-import { fal } from '@fortawesome/pro-light-svg-icons';
-import { fad } from '@fortawesome/pro-duotone-svg-icons';
-import { far } from '@fortawesome/pro-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 
-// Load icons into
-library.add(fat, fal, fas, fad, far, fab);
+// Load icons into the library
+library.add(fas, far, fab);
 
 /**
  * Icon factory utility.
